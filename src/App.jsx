a4e7a211338944fc930c5a5a348813f33cbe7ded
restuadil <restuadil09@gmail.com>
2023-11-15 17:57:09 +0700

    add path

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+
+
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+        </Routes>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,12 @@
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import Login from "./pages/Login/Login";
-import Home from "./pages/Home/Home";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App';
+import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/",
-    element: <Home />
-  }
-]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
