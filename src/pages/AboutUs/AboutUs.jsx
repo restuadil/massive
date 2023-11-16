2bdd97d1f2e25e5ba416eefae6ace239e620a161
restuadil15 <restuadil09@gmail.com>
2023-11-17 00:44:59 +0700

    add page aboutus

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -19,16 +19,18 @@ const AboutUs = () => {
                 </h2>
                 <p className="text-white font-poppins font-light text-xs tracking-wide md:mb-4">Pengasuh Hewan Peliharaan yang Berpengalaman dan Penyayang Hewan</p>
             </Hero>
-            <div className="mx-5">
-                <img src="img/hero.jpeg" className="h-[500px] block mx-auto p-10" alt="" />
-                <h1 className="font-poppins font-bold text-2xl text-center">Kami Menyediakan Layanan Perawatan Hewan Peliharaan Selama <span className="text-red">Bertahun-Tahun</span></h1>
-                <p className="font-montserrat mt-5 text-justify">“Di Pet Care, tujuan kami adalah membuat perawatan hewan peliharaan menjadi lebih mudah, dengan memberikan layanan yang fleksibel dan dapat diandalkan, sehingga pemilik hewan peliharaan dapat menjalankan bisnisnya, dan tidak perlu khawatir hewan peliharaannya menunggu di rumah. "</p>
-                <span className="font-montserrat font-bold mt-8 block">Founder, Pet Care</span>
-                <span className="font-montserrat font-bold">Bobby</span>
+            <div className="mx-5 md:flex md:flex-row md:items-center">
+                <img src="img/hero.jpeg" className="h-[500px] md:w-1/2  block mx-auto p-10" alt="" />
+                <div>
+                    <h1 className="font-poppins font-bold text-2xl text-center">Kami Menyediakan Layanan Perawatan Hewan Peliharaan Selama <span className="text-red">Bertahun-Tahun</span></h1>
+                    <p className="font-montserrat mt-5 text-justify">“Di Pet Care, tujuan kami adalah membuat perawatan hewan peliharaan menjadi lebih mudah, dengan memberikan layanan yang fleksibel dan dapat diandalkan, sehingga pemilik hewan peliharaan dapat menjalankan bisnisnya, dan tidak perlu khawatir hewan peliharaannya menunggu di rumah. "</p>
+                    <span className="font-montserrat font-bold mt-8 block">Founder, Pet Care</span>
+                    <span className="font-montserrat font-bold">Bobby</span>
+                </div>
             </div>
-            <div className="bg-cover bg-no-repeat px-5 py-10" style={{ backgroundImage: `url("img/bg.png")` }}>
-                <h1 className=" font-poppins font-bold text-2xl">Hewa Peliharaan Anda <span className="text-red">Aman</span> Bersama Kami</h1>
-                <div className="flex flex-col gap-5 mt-5">
+            <div className="bg-cover bg-no-repeat px-5 py-10 " style={{ backgroundImage: `url("img/bg.png")` }}>
+                <h1 className=" font-poppins font-bold text-2xl md:text-center md:py-1">Hewa Peliharaan Anda <span className="text-red">Aman</span> Bersama Kami</h1>
+                <div className="flex flex-col gap-5 mt-5 md:flex md:flex-row md:items-center">
                     <div className="flex flex-col gap-3">
                         <FaMedal className="text-red text-3xl" />
                         <h1 className="text-red font-poppins font-bold">Staf Yang Terlatih</h1>
@@ -51,18 +53,20 @@ const AboutUs = () => {
                     </div>
                 </div>
             </div>
-            <div>
-                <h1 className=" font-poppins font-bold text-2xl text-center mt-3">Rumah Anda <span className="text-red">Terlindungi</span></h1>
-                <p className="font-montserrat mx-5 tracking-wide block mt-5 text-justify text-sm">Kami tahu bahwa setelah kesehatan, keselamatan, dan kebahagiaan hewan peliharaan Anda, keamanan rumah Anda adalah prioritas utama. Inilah cara kami memastikan keamanan rumah Anda saat Anda bepergian:</p>
-                <ul className="list-disc mx-10 mt-3 text-justify font-montserrat">
-                    <li>Satu-satunya orang yang memiliki akses ke kunci atau kode akses Anda adalah kantor utama lokasi Anda dan pengasuh Anda
-                    </li>
-                    <li>Kami melacak semua pergerakan kunci, artinya jika pengasuh Anda memerlukan kunci, mereka diharuskan mengajukan permintaan di pelacakan aplikasi sistem kami
-                    </li>
-                    <li>Jika rumah Anda memiliki kode akses, kode tersebut hanya akan tersedia bagi pengasuh Anda selama jam layanan terjadwal`</li>
-                </ul>
+            <div className="md:flex md:flex-row-reverse md:items-center">
+                <div>
+                    <h1 className=" font-poppins font-bold text-2xl text-center mt-3">Rumah Anda <span className="text-red">Terlindungi</span></h1>
+                    <p className="font-montserrat mx-5 tracking-wide block mt-5 text-justify text-sm">Kami tahu bahwa setelah kesehatan, keselamatan, dan kebahagiaan hewan peliharaan Anda, keamanan rumah Anda adalah prioritas utama. Inilah cara kami memastikan keamanan rumah Anda saat Anda bepergian:</p>
+                    <ul className="list-disc mx-10 mt-3 text-justify font-montserrat">
+                        <li>Satu-satunya orang yang memiliki akses ke kunci atau kode akses Anda adalah kantor utama lokasi Anda dan pengasuh Anda
+                        </li>
+                        <li>Kami melacak semua pergerakan kunci, artinya jika pengasuh Anda memerlukan kunci, mereka diharuskan mengajukan permintaan di pelacakan aplikasi sistem kami
+                        </li>
+                        <li>Jika rumah Anda memiliki kode akses, kode tersebut hanya akan tersedia bagi pengasuh Anda selama jam layanan terjadwal`</li>
+                    </ul>
+                </div>
+                <img src="img/hero.jpeg" className="h-[500px] block mx-auto p-10 md:w-1/2" alt="" />
             </div>
-            <img src="img/hero.jpeg" className="h-[500px] block mx-auto p-10" alt="" />
             <Review bg={"img/bg.png"} />
             <div style={{ backgroundImage: `url("img/bg2.png")` }} className="bg-center bg-cover bg-no-repeat ">
                 <Request />
